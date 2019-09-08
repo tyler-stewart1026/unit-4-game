24d5e6fdc5991a96d24960e5daf258aae9aeac36
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-09-08 16:46:04 -0600

    redid crystal variable and finished functional side of game. styling is all that's left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,31 +8,119 @@ var randNum = 0;
 // total number of user
 var totalNum = 0;
 // values of crystals
-var crystalOneVal = 0;
-var crystalTwoVal = 0;
-var crystalThreeVal = 0;
-var crystalFourVal = 0;
-
+var crystal = {
+    one:
+    {
+      name: "Blue",
+      value: 0
+    },
+    two:
+    {
+      name: "Green",
+      value: 0
+    },
+    three:
+    {
+      name: "Red",
+      value: 0
+    },
+    four:
+    {
+      name: "Yellow",
+      value: 0
+    }
+  };
 // Functions==========
 
 // Create variable that is a function to generate random numbers
-var getRandom = function(min, max) {
+var getRandom = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-// Start game function
-function gameStart() {
+// Start game function ----- this might also work as a reset. 
+var gameStart = function() {
     // assign random number. 
     randNum = getRandom(19, 120);
     console.log(randNum)
     // assign each cryssle a number. 
-    crystalOneVal = getRandom(1, 12);
-    crystalTwoVal = getRandom(1, 12);
-    crystalThreeVal = getRandom(1, 12);
-    crystalFourVal = getRandom(1, 12);
-    console.log("cryssle 1: " + crystalOneVal + ", Cryssle 2: " + crystalTwoVal + ", cryssle 3: " + crystalThreeVal + ",  cryssle 4: " + crystalFourVal);
+    crystal.one.value = getRandom(1, 12);
+    crystal.two.value = getRandom(1, 12);
+    crystal.three.value = getRandom(1, 12);
+    crystal.four.value = getRandom(1, 12);
+    console.log("cryssle 1: " + crystal.one.value + ", Cryssle 2: " + crystal.two.value + ", cryssle 3: " + crystal.three.value + ",  cryssle 4: " + crystal.four.value);
     //set total number to 0 (possible act as a reset?)
     totalNum = 0;
-    $("#randomNumber").text(randNum);
+    $("#randomNumber").text("Target number: " + randNum);
+    $(".totalScore").text("Your total score: " + totalNum);
 };
+
+// Check if user won or lost and reset game
+var checkWin = function () {
+
+    // Check if total number is larger than random number
+    if (totalNum > randNum) {
+        alert("Sorry. You lost!");
+        console.log("You Lost");
+
+        // Add to losses
+        losses++;
+
+        // Change Loss Count 
+        $("#losses").text("Loss: " + losses);
+
+        // Restart the game
+        gameStart();
+    }
+
+    else if (totalNum === randNum) {
+        alert("Congratulations! You Won!");
+        console.log("You Won!");
+
+        // Add to wins
+        wins++;
+
+        // Change Win Count HTML
+        $("#wins").text("Wins: " + wins);
+
+        // Restart the game
+        gameStart();
+    }
+
+};
+
+// Adding a function for the crystals to be clicked and add number
+var values = function (crystalClick) {
+
+    // change totalNum
+    totalNum += crystalClick.value;
+
+    $(".totalScore").text("Your total score: " + totalNum);
+
+    // call the check win 
+    checkWin();
+
+    console.log("Your score so far: " + totalNum);
+};
+
+// Main======
+
+//Main application
+
 gameStart();
+
+$(".crystal1").click(function () {
+    values(crystal.one);
+    console.log(crystal.one);
+});
+
+$(".crystal2").click(function () {
+    values(crystal.two);
+});
+
+$(".crystal3").click(function () {
+    values(crystal.three);
+});
+
+$(".crystal4").click(function () {
+    values(crystal.four);
+});
